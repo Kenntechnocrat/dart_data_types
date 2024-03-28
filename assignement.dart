09b739aed768e7448993eb82209df2b3017ee930
Kenntechnocrat <oumakenn21@gmail.com>
2024-03-28 20:46:54 +0300

    first commit

---
+++ assignement.dart
@@ -0,0 +1,13 @@
+void main(){
+// string example
+String name="Kenn";
+print('I am $name');
+//map example
+Map<String,double> car_prices={
+  'BMW':6500000.00,'Lexus':15000000.00,'Volvo':990000.05};
+
+print('Car prices in USA$car_prices');
+//LIST
+List<int>lucky_numbers=[1,3,4,7,0];
+print('The lucy numbers are $lucky_numbers');
+}
